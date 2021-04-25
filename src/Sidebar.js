e6d0296c4785ae01ae3a3e3a1adaac9318d69ffe
MrGanesh <ganeshkulkarni689@gmail.com>
2021-04-25 15:38:31 +0530

    2 files modified

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -26,8 +26,9 @@ function Sidebar() {
       justifyContent: "center"
     },
     paper: {
-      height: '104px',
-     width: '250px',
+      height: '135px',
+     width: '-webkit-fill-available',
+      maxWidth: '300px',
     // padding: '16px 32px 24px',
     position: 'fixed',
     boxShadow: '0px 3px 5px -1px rgb(0 0 0 / 20%), 0px 5px 8px 0px rgb(0 0 0 / 14%), 0px 1px 14px 0px rgb(0 0 0 / 12%)',
